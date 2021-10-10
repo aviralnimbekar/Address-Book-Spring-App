22a28a9ad5ec747d2712b7135b1d5b60ab04374e
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-10-10 23:51:00 +0530

    ADDED : Conection with DB and Started project

---
+++ src/main/java/com/bridgelabz/addressbookspringapp/AddressBookSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbookspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressBookSpringAppApplication.class, args);
+    }
+
+}
